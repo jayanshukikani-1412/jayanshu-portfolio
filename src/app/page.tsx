46debeecea2e017a26a16c517ac9beb330610b2a
Jayanshu-CrawlApps <jayanshu.crawlapps@gmail.com>
2024-12-05 07:57:46 +0530

    Feat: add portfolio link in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-r from-blue-800 to-indigo-900">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-b from-gray-900 to-gray-600">
       <h1 className="text-[140px] font-bold text-white text-balance">JAYANSHU</h1>
       <h2 className="text-[140px] font-bold text-white text-balance">KIKANI</h2>
+      <Link href={"https://www.stefanobartoletti.it/"} className="text-white text-balance text-[40px]">PORTFOLIO LINK</Link>
     </main>
   );
 }
